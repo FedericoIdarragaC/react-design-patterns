dbb3665f54dd19130b8d81eb9e45d43935072dd4
FedericoIdarragaC <federico.idarraga@utp.edu.co>
2023-03-14 09:21:13 -0500

    Functional Programming

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 
+import CC from './ComponentComposition/CC';
 import ContainerComponents from './ContainerComponents/ContainerComponents';
 import CustomHooks from './CustomHooks/CustomHooks';
 import HighOrderComponents from './HighOrderComponents/HighOrderComponents';
 import LayoutComponent from './LayoutComponents/LayoutComponets';
 import Lists from './Lists/Lists';
 import Modals from './ModalComponents/Modals';
+import RC from './RecursiveComponents/RC';
 import UncontrolledControlledComponents from './Uncontrolled&ControlledComponents/Uncontrolled&ControlledComponents';
 
 function App() {
   return (
-    <CustomHooks />
+    <CC />
   );
 }
 
---
+++ src/ComponentComposition/CC.js
@@ -0,0 +1,10 @@
+import { BigSuccessButton, DangerButton } from "./partiallyApply"
+
+function CC() {
+  return <>
+    <DangerButton text="Danger!!" />
+    <BigSuccessButton text="Yes!!" />
+  </>
+}
+
+export default CC
